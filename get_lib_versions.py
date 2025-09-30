42452bdd065779b0e94c8a2847c6e7f565a7b770
MAHI <uuma8628@gmail.com>
2025-09-30 17:34:23 +0100

    first commit

---
+++ get_lib_versions.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "langchain_core",
+    "python-dotenv" 
+    ]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ecommerce-prod-assistant!")
+
+
+if __name__ == "__main__":
+    main()
